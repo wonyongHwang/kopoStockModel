c1dfd9eefdcd1fa01dcf6ff1b3fb8fb92179dfac
wayne hwang <wonyong@wonyong.net>
2022-05-10 09:37:01 +0900

    font setting modified

--- lecture/02 sample test.py
+++ lecture/02 sample test.py
@@ -2,12 +2,16 @@ import FinanceDataReader as web
 from datetime import date, timedelta
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
-import datetime
+import platform
 
-# plt.figure(figsize=(16,32))
 from matplotlib import font_manager, rc
-font_name = font_manager.FontProperties(fname="c:/Windows/Fonts/Art.ttf").get_name()
-# rc('font', family='AppleGothic')
+if platform.system() == 'Windows':
+    font_name = font_manager.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
+    rc('font', family=font_name)
+elif platform.system() == 'Darwin':
+    rc('font', family='AppleGothic')
+else:
+    pass
 
 today = date.today()
 startday = date.today() - timedelta(720)
@@ -24,7 +28,7 @@ plt.title("Price Chart")
 plt.ylim([600000, 1100000])     # Y축의 범위: [ymin, ymax]
 plt.gca().yaxis.set_major_formatter(mticker.FormatStrFormatter('%d'))
 
-SEC["2020-05-06":"2022-05-06"]['Close'].plot(figsize=(16,10), style='b',xlabel='Date', ylabel='Close Price')
+SEC["2020-05-06":"2022-05-06"]['Close'].plot(figsize=(16,10), style='b',xlabel='Date', ylabel='종가')
 plt.subplots_adjust(hspace=0.5)
 
 plt.subplot(212)
